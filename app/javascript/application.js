f3ac5b5e7fdafc1d3d814ef21167f6613545bbe9
Hassan Nawaz <hassannawaz412@gmail.com>
2022-05-17 16:22:21 +0500

    changing made for working bootstrap working correctly

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,13 @@
 //= require jquery
-//= require jquery_ujs
-//= require jquery
+//= require rails-ujs
 //= require bootstrap-sprockets
-//= require turbolinks
+//= require bootstrap
 //= require_tree .
+
+import 'bootstrap'
+require ("@rails/ujs").start()
+require ("turbolinks").start()
+require ("@rails/activestorage").start()
+require ("channels")
+require ("packs/custom")
+require ("packs/post.js")
